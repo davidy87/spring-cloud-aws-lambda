e7528ed34d423e593e45ff9f352ff9e2988ddef9
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-04-24 22:25:12 +0900

    setup project

---
+++ src/main/java/demo/springcloud/lambda/SpringCloudLambdaApplication.java
@@ -0,0 +1,13 @@
+package demo.springcloud.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/springcloud/lambda/SpringCloudLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springcloud.lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
